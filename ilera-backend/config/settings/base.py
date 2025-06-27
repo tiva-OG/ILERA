928165bb386b21cf013d1ff155a7bb09fea0f67c
tiva-OG <tivasalvation@gmail.com>
2025-06-27 10:47:19 +0100

    switched to prod

--- ilera-backend/config/settings/base.py
+++ ilera-backend/config/settings/base.py
@@ -141,8 +141,8 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
